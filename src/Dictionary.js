2dcb4b8bd10f6b4179de93440c115a457109bf93
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-05-21 22:05:11 +1000

    integrated photo display

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result"
 import './dictionary.css';
+import Photos from "./Photos";
 
 export default function Dictionary() {
 let [input, setInput] = useState("nostalgia")
 let [result, setResult] = useState(null)
 let [inputReceived, setInputReceived] = useState(null)
+let [photos, setPhotos] = useState(null)
 
 
 function load() {
@@ -19,10 +21,26 @@ function handleInput(event){
     setInput(event.target.value)
 }
 
+function handlePexels(response) {
+    console.log(response)
+    setPhotos(response.data.photos)
+}
+
 function search () {
     let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`
 
-    axios.get(apiUrl).then(handleResponse)
+    
+
+    const pexelKey= "563492ad6f917000010000012add3367c8794fb98ceeed89c3565ce0"
+    let pexelApi= `https://api.pexels.com/v1/search?query=${input}&per_page=9`
+
+    axios.get(apiUrl).then(handleResponse)    
+    axios
+    .get(pexelApi, {
+      headers: { Authorization: `Bearer ${pexelKey}` },
+    })
+    .then(handlePexels);
+    
 }
 
 function handleResponse(response){
@@ -47,6 +65,7 @@ function handleResponse(response){
     </section>
   
     <Result results={result}/>
+    <Photos photos={photos} />
     
     </div>
    
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./synonyms.css"
 
 export default function Synonyms(props) {
-    console.log(props)
     if(props.synonyms) { return (
         <ul className="Synonyms">
 {props.synonyms.map(function(synonym,index) {
