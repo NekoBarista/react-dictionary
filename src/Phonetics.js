033e0e5cb661c75f5137999ff54164de45682799
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-05-21 17:14:57 +1000

    phonetics imbedded

---
+++ src/Phonetics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Phonetics(props) {
+    return (
+        <div className="Phonetics">
+{props.phonetic.text} <br />
+<a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a>
+
+
+
+</div>  )
+}
